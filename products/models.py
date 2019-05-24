74bd635815efaec54f39892c535f4f1a59ab7d66
Larrygf02 <raulgf_02@hotmail.com>
2019-05-24 15:55:52 -0500

    Mas de un manager en un modelo

--- products/models.py
+++ products/models.py
@@ -14,4 +14,33 @@ class Book(models.Model):
     def __str__(self):
         return self.author +" "+ self.title
 
+class MaleManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(gender='M')
+
+class FemaleManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(gender='F')
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    MALE = 'M'
+    FEMALE = 'F'
+    GENDER_OPTIONS = (
+        (MALE, 'Masculino'),
+        (FEMALE, 'Femenino')
+    )
+    gender = models.CharField(max_length=2, choices=GENDER_OPTIONS, default=MALE)
+    #Managers
+    objects = models.Manager()
+    males = MaleManager()
+    females = FemaleManager()
+
+    #create and update
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.first_name + " "+ self.last_name
+
 
