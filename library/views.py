592fe1377163ed52743671c6b89e300d24d35452
larrygf02 <raulgf_02@hotmail.com>
2019-05-27 22:05:00 -0500

    generate pdf

--- library/views.py
+++ library/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+import io
+from django.http import FileResponse
+from reportlab.pdfgen import canvas
+
+def some_view(request):
+    buffer = io.BytesIO()
+    p = canvas.Canvas(buffer)
+
+    p.drawString(100,100, "Hello World")
+    p.showPage()
+    p.save()
+
+    return FileResponse(buffer, as_attachment=True, filename='hello.pdf')
